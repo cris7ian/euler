434a6214ed3464c3ae524be9bdcb98c39e86d1bc
Cristian Caroli <cristiancaroli@gmail.com>
2015-05-26 18:08:27 +0200

    Not using lodash for this one.

--- src/problems/1.js
+++ src/problems/1.js
@@ -4,7 +4,6 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Find the sum of all the multiples of 3 or 5 below 1000.
 */
 module.exports = () => {
-  var _ = require('lodash');
   var sum = 0;
   for (var i = 0; i < 1000; i++) {
     if ((i % 3 === 0) || (i % 5 == 0)) sum += i;
