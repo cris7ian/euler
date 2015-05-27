690027059ecadb26c1d9524beb9494a553e47bd8
Cristian Caroli <cristiancaroli@gmail.com>
2015-05-27 09:40:23 +0200

    5.

--- src/solutions/5.js
+++ src/solutions/5.js
@@ -4,7 +4,17 @@
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 */
 module.exports = () => {
-  var answer = 0;
+  function isMCD(n) {
+    for (var i = 1; i < 20; i++) if (n % i != 0) return false;
+
+    return true;
+  }
+
+  var answer = 20; //Can't be smaller than 20.
+  while (true) {
+      if (isMCD(answer)) break;
+      answer++;
+  }
 
   return answer;
 }
